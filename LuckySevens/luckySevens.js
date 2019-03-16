891add433cafbc77aba33a58b37cfac6fc8b7ebd
Robert Esquivel <rob_e_1008ad@yahoo.com>
2019-03-16 17:35:19 -0500

    function created for rolling 2 dice; need to display on main page

--- LuckySevens/luckySevens.js
+++ LuckySevens/luckySevens.js
@@ -1,1 +1,10 @@
 /* Javascript code for luckySevens file goes here */
+
+/* function for rolling dice*/
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+ 
+for (var i = 0; i < 2; i++) {
+  console.log(rollDice());
+}
