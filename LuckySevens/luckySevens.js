a4f24b7c8b6f03b2f9e2a8ee1ae5e47272b43010
Robert Esquivel <rob_e_1008ad@yahoo.com>
2019-03-16 19:16:02 -0500

    added clearErrors function and modified form class

--- LuckySevens/luckySevens.js
+++ LuckySevens/luckySevens.js
@@ -1,5 +1,8 @@
 /* Javascript code for luckySevens file goes here */
 
+
+var startingBet = 
+
 /* function for rolling dice*/
 function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
@@ -8,3 +11,5 @@ function rollDice() {
 for (var i = 0; i < 2; i++) {
   console.log(rollDice());
 }
+
+/*get result1 and result 2 from diceRoll results and sum together*/
