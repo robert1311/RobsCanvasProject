c89f1296c31034ac1a354d32c6c52b9394238b37
Robert Esquivel <rob_e_1008ad@yahoo.com>
2019-03-17 20:56:35 -0500

    validate forms and results table updated

--- LuckySevens/luckySevens.js
+++ LuckySevens/luckySevens.js
@@ -32,9 +32,7 @@
 //		}
 //	}
 		
-		
-
-function clearErrors() {    
+	function clearErrors() {   
     for (var loopCounter = 0; 
         loopCounter < document.forms["startingBet"].elements.length; 
         loopCounter++) {
@@ -49,42 +47,42 @@ function clearErrors() {
 
 function resetForm() {
     clearErrors();
-    document.forms["startingBet"]["num"].value = "";
+    document.forms["startingBet"]["num"].value = "$0.00";
     document.getElementById("results").style.display = "none";
     document.getElementById("submitButton").innerText = "Play";
     document.forms["startingBet"]["num"].focus();
-	
+}
+
 function validateItems() {
     clearErrors();
-    var num = document.forms["numberFun"]["num1"].value;
-   // var num2 = document.forms["numberFun"]["num2"].value;
-    if (num == "" || isNaN(num)) {
-        alert("Num must be filled in with a number.");
+    var money = document.forms["startingBet"]["num"].value;
+    if (money == "" || isNaN(money)) {
+        alert("num must be filled in with a positive integer.");
         document.forms["startingBet"]["num"]
            .parentElement.className = "form-group has-error";
         document.forms["startingBet"]["num"].focus();
         return false;
     }
-	
-	
-  // if (num2 == "" || isNaN(num2)) {
-    //   alert("Num2 must be filled in with a number.");
-      // document.forms["numberFun"]["num2"]
-        //  .parentElement.className = "form-group has-error"
-       //document.forms["numberFun"]["num2"].focus();
-       //return false;
+   if (num2 == "" || isNaN(num2)) {
+       alert("Num2 must be filled in with a number.");
+       document.forms["numberFun"]["num2"]
+          .parentElement.className = "form-group has-error"
+       document.forms["numberFun"]["num2"].focus();
+       return false;
    }
-  // document.getElementById("results").style.display = "block"; 
-  //document.getElementById("submitButton").innerText = "Recalculate";
-  // document.getElementById("addResult").innerText = Number(num1) +
- //                                                    Number(num2);
-//   document.getElementById("subtractResult").innerText = num1 - num2;
-//   document.getElementById("multiplyResult").innerText = num1 * num2;
-//   document.getElementById("divideResult").innerText = num1 / num2;
+   document.getElementById("results").style.display = "block";
+   document.getElementById("submitButton").innerText = "Recalculate";
+   document.getElementById("addResult").innerText = Number(num1) +
+                                                     Number(num2);
+   document.getElementById("subtractResult").innerText = num1 - num2;
+   document.getElementById("multiplyResult").innerText = num1 * num2;
+   document.getElementById("divideResult").innerText = num1 / num2;
    // We are returning false so that the form doesn't submit 
    // and so that we can see the results
-//   return false;
-//}	
+   return false;
+}	
+
+
 
 /* function for rolling dice*/
 /*function rollDice() {
